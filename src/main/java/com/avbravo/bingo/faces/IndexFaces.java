932c58a9199edd16bd9bf2e56698614e2d62b7a7
avbravo <avbravo@avbravo>
2022-12-06 16:34:37 -0500

    0.5

---
+++ src/main/java/com/avbravo/bingo/faces/IndexFaces.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.avbravo.bingo.faces;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author avbravo
+ */
+@Named(value = "indexFaces")
+@Dependent
+public class IndexFaces {
+
+    /**
+     * Creates a new instance of IndexFaces
+     */
+    public IndexFaces() {
+    }
+    
+}
