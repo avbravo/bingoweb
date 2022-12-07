29f36f96a8cde48b15024a68a577a222d6c12d99
avbravo <avbravo@avbravo>
2022-12-06 21:00:52 -0500

    index

--- src/main/java/com/avbravo/bingo/faces/IndexFaces.java
+++ src/main/java/com/avbravo/bingo/faces/IndexFaces.java
@@ -7,6 +7,8 @@ package com.avbravo.bingo.faces;
 import com.avbravo.jmoordbutils.JsfUtil;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.inject.Named;
 import java.io.Serializable;
 import lombok.Data;
@@ -37,5 +39,60 @@ public class IndexFaces implements Serializable{
         JsfUtil.warningMessage("Hola Mundo");
         System.out.println("llego al Hello.....");
         return "";
+    }
+    
+    
+    private Integer progress1;
+    private Integer progress2;
+
+    public void longRunning() throws InterruptedException {
+        setProgress2(0);
+        Integer k = getProgress2();
+        while (k == null || k < 100) {
+            k = updateProgress(k);
+            setProgress2(k);
+            Thread.sleep(500);
+        }
+    }
+
+    private static Integer updateProgress(Integer progress) {
+        if (progress == null) {
+            progress = 0;
+        }
+        else {
+            progress = progress + (int) (Math.random() * 35);
+
+            if (progress > 100) {
+                progress = 100;
+            }
+        }
+
+        return progress;
+    }
+
+    public void onComplete() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Progress Completed"));
+    }
+
+    public void cancel() {
+        progress1 = null;
+        progress2 = null;
+    }
+
+    public Integer getProgress1() {
+        progress1 = updateProgress(progress1);
+        return progress1;
+    }
+
+    public Integer getProgress2() {
+        return progress2;
+    }
+
+    public void setProgress1(Integer progress1) {
+        this.progress1 = progress1;
+    }
+
+    public void setProgress2(Integer progress2) {
+        this.progress2 = progress2;
     }
 }
